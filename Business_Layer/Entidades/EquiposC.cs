b4bd42edb6c4e158a8f3cdf877bbce47db9fa50c
Mario A. <mac132@live.com.mx>
2024-03-25 16:41:38 -0600

    [Add] form devices

---
+++ Business_Layer/Entidades/EquiposC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DataAccess_Layer;
+
+namespace Business_Layer.Entidades
+{
+    public class EquiposC
+    {
+        private Data data = Data.Instance();
+
+        public String Codigo { get; set; }
+        public int Marca { get; set; }
+        public String Modelo { get; set; }
+        public DateTime Fecha_Compra { get; set; }
+
+        public EquiposC() { }
+
+        
+
+    }
+}
